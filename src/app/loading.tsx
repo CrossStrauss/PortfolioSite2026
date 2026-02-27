0dfe42713ed921718da4ee764658f72805dcb900
CrossStrauss <chriscross2501@gmail.com>
2026-02-27 08:57:41 +0200

    Layout loader and adjustments

---
+++ src/app/loading.tsx
@@ -0,0 +1,23 @@
+export default function Loading() {
+  return <div className="fixed top-0 right-0 w-full h-screen flex items-center justify-center bg-gray-900 z-3">
+    <div className="relative">
+
+
+      <div className="absolute size-20 top-0 left-0 rounded-full bg-white flex justify-center items-center relative border border-gray-900">
+        {/* <div className="w-[90%] h-[90%] rounded-full bg-gray-900">
+        </div>
+        <div className="w-[60%] rounded-r-full h-full absolute bg-gray-900 translate-x-[50%]">
+        </div> */}
+      </div>
+
+
+      <div className="absolute size-20 top-0 left-0 rounded-full bg-white flex justify-center items-center relative border border-gray-900">
+        {/* <div className="w-[90%] h-[90%] rounded-full bg-gray-900">
+        </div>
+        <div className="w-[60%] rounded-r-full h-full absolute bg-gray-900 translate-x-[50%]">
+        </div> */}
+      </div>
+
+    </div>
+  </div>;
+}
